f8f99b4865556738052e9cd2214aa94c852c72cf
paradoxinfinite <10831629+ParadoxInfinite@users.noreply.github.com>
2020-07-26 03:15:42 +0530

    Rust and JS computation + comparision

--- node/app.js
+++ node/app.js
@@ -1,19 +1,44 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+const { number_details } = require("../pkg/ssvm_nodejs_starter_lib.js");
+const express = require("express");
 
-const http = require('http');
-const url = require('url');
-const hostname = '0.0.0.0';
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
-  } else {
-    res.end(say(queryObject['name']) + '\n');
-  }
+app.use(express.static(__dirname + "/public"));
+console.log(__dirname);
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.redirect("/index.html"));
+
+app.post("/number-details", async function (req, res) {
+  const number = parseFloat(req.body.number);
+  const rust_result = number_details(number);
+  const js_result = number_details_js(number);
+  res.send(rust_result + js_result);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+
+function find_factors(number) {
+  return [...Array(number + 1).keys()].filter((i) => number % i === 0);
+}
+
+function number_details_js(number) {
+  const start_time = process.hrtime();
+  const square = number * number;
+  const cube = square * number;
+  const square_root = Math.sqrt(number);
+  const cube_root = Math.cbrt(number);
+  const temp_factors = find_factors(number);
+  const end_time = process.hrtime(start_time);
+  const time_elapsed = `${end_time[0]}s ${(end_time[1] / 1000000).toFixed(
+    2
+  )}ms`;
+  const factors = temp_factors.join(", ");
+
+  return `<hr/><strong>JAVASCRIPT RESULTS</strong><hr/>Square = ${square}<br/>Cube = ${cube}<br/>Square Root = ${square_root.toFixed(
+    2
+  )}<br/>Cube Root = ${cube_root.toFixed(
+    2
+  )}<br/> Factors = [${factors}]<br/><hr/>Time elapsed for the operation = ${time_elapsed}<hr/>`;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,25 @@
 use wasm_bindgen::prelude::*;
+use std::time::SystemTime;
 
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+pub fn number_details(number: i32) -> String {
+  println!("The Rust function numberDetails received {}", number);
+  let start = SystemTime::now();
+  let float_number: f64 = number as f64;
+  let mut result: (f64, f64, f64, f64, Vec<i32>) = (0., 0., 0., 0., [].to_vec());
+  result.0 = float_number * float_number;
+  result.1 = result.0 * float_number;
+  result.2 = float_number.sqrt();
+  result.3 = float_number.cbrt();
+  result.4 = get_factors_functional(number);
+  let end = SystemTime::now();
+  let time_elapsed;
+  match end.duration_since(start) {
+    Ok(n) => time_elapsed = n,
+    Err(_) => panic!("Cannot determine time."),
+}
+  return format!("<hr/><strong>RUST RESULTS</strong><hr/>Square = {}<br/>Cube = {}<br/>Square Root = {:.2}<br/>Cube Root = {:.2}<br/> Factors = {:?}<br/><hr/>Time elapsed for the operation = {:.2?}<hr/>", result.0, result.1,result.2, result.3, result.4, time_elapsed)
+}
+fn get_factors_functional(n: i32) -> Vec<i32> {
+    (1..n + 1).into_iter().filter(|&x| n % x == 0).collect::<Vec<i32>>()
 }
